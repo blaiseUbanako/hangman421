cdd1213694b108f247424286ca836e0b6f1d96c7
blaise <blaise.ubanako@gmail.com>
2024-05-22 14:46:19 +0100

    refactor by adding resusable methods

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -3,15 +3,20 @@ import random as rd
 
 word_list = ["apple", "banana", "mango", "orange", "strawberry" ]
 
-for fruits in word_list:
-    print(fruits)
+def random_fruit():
+   for fruits in word_list:
+     print(fruits)
+     word = rd.choice(word_list)
+     print(word)
 
-word = rd.choice(word_list)
-print(word)
+
+def guess_alphabet():
+      guess = input('Please enter a single letter: ')
+      if len(guess) ==1 and guess.isalpha():
+       print("Good guess")
+      else:
+       print("Oops! That is not a valid input.")
 
 
-guess = input('Please enter a single letter: ')
-if len(guess) ==1 and guess.isalpha():
- print("Good guess")
-else:
- print("Oops! That is not a valid input.")
+random_fruit()
+guess_alphabet()
