46e6a794c381ded552816f8da926dd6cda88dab0
blaise <blaise.ubanako@gmail.com>
2024-05-27 08:27:22 +0100

    code refactor

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,4 +1,3 @@
-from milestone_2 import word_list
 from milestone_2 import random_fruit
 
 
@@ -10,13 +9,15 @@ def check_guess(guess):
   lower_case = guess.lower()
   if lower_case in selected_fruit:
       print(lower_case, "is in", selected_fruit)
+  else:
+    print(lower_case, "is not in", selected_fruit)
 
 
 def ask_for_input():
     while True:
-     my_input = input("Guess an alphabet: ")
-     if len(my_input) == 1 and my_input.isalpha:
-       print(f"{my_input} is a good guess")
+     new_input = input("Guess an alphabet: ")
+     if len(new_input) == 1 and new_input.isalpha:
+       print(f"{new_input} is a good guess")
      else:
       print("Invalid letter. Please, enter a single alphabetical character.")
      
